acb7a3d8fae4a844459be785e15a41d07cbe7ed1
Dario Carlino <baires82@gmail.com>
2020-09-21 16:58:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,21 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
-    @twitter = student_hash[:twitter]
-    @linkedin = student_hash[:linkedin]
-    @github = student_hash[:github]
-    @blog = student_hash[:blog]
-    @profile_quote = student_hash[:profile_quote]
-    @bio = student_hash[:bio]
-    @profile_url = student_hash[:profile_url]
+    send("twitter=", attributes_hash[:twitter])
+    send("linkedin=", attributes_hash[:linkedin])
+    send("github=", attributes_hash[:github])
+    send("blog=", attributes_hash[:blog])
+    send("bio=", attributes_hash[:bio])
+    send("profile_quote=", attributes_hash[:profile_quote])
+    # @name = student_hash[:name]
+    # @location = student_hash[:location]
+    # @twitter = student_hash[:twitter]
+    # @linkedin = student_hash[:linkedin]
+    # @github = student_hash[:github]
+    # @blog = student_hash[:blog]
+    # @profile_quote = student_hash[:profile_quote]
+    # @bio = student_hash[:bio]
+    # @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
