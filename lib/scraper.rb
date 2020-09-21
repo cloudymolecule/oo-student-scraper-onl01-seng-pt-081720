808c683de244f2982f8096cdd7990173624dc3ac
Dario Carlino <baires82@gmail.com>
2020-09-21 17:02:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    array = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css("div.student-card")
+    students.each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile = student.css("a").attribute("href").value
+      temp = {name: name, location: location, profile_url: profile}
+      array << temp
+
+    end
+    array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    hash = {}
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    socials = doc.css(".social-icon-container a")
+    socials.each do |prof|
+      social = prof.attribute("href").value
+      if social.include?("twitter")
+        hash[:twitter] = social
+      elsif social.include?("linkedin")
+        hash[:linkedin] = social
+      elsif social.include?("github")
+        hash[:github] = social
+      elsif social.include?("http")
+        hash[:blog] = social
+      end
+    end
+    hash[:profile_quote] =  doc.css(".profile-quote").text
+    hash[:bio] = doc.css(".description-holder p").text
+    hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,38 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    send("name=", student_hash[:name])
+    send("location=", student_hash[:location])
+    send("twitter=", student_hash[:twitter])
+    send("linkedin=", student_hash[:linkedin])
+    send("github=", student_hash[:github])
+    send("blog=", student_hash[:blog])
+    send("bio=", student_hash[:bio])
+    send("profile_quote=", student_hash[:profile_quote])
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      new_student = Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    send("twitter=", attributes_hash[:twitter])
+    send("linkedin=", attributes_hash[:linkedin])
+    send("github=", attributes_hash[:github])
+    send("blog=", attributes_hash[:blog])
+    send("bio=", attributes_hash[:bio])
+    send("profile_quote=", attributes_hash[:profile_quote])
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
