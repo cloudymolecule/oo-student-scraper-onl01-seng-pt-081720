6680bbb6b69cd4c208c828227fd36c19c8bacef4
Dario Carlino <baires82@gmail.com>
2020-09-21 16:46:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      binding.pry
+      new_student = Student.new(student)
     end
     #binding.pry
   end
