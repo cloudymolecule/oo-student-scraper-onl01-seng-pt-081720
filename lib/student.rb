28914349061798078104909c557dc7779d57d470
Dario Carlino <baires82@gmail.com>
2020-09-21 16:35:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,6 @@ class Student
     @profile_quote = student_hash[:profile_quote]
     @bio = student_hash[:bio]
     @profile_url = student_hash[:profile_url]
-    binding.pry
   end
 
   def self.create_from_collection(students_array)
