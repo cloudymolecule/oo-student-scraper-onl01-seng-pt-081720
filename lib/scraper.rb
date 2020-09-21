3237b90bcf2dc37b72c14516f8e557169efb5525
Dario Carlino <baires82@gmail.com>
2020-09-21 14:07:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,10 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
-    students = doc.css("body > div > div > div.roster-cards-container").text
+    students = doc.css("body > div > div > div.roster-cards-container")
     students.each do |student|
       binding.pry
     end
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
