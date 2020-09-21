f825a94aef3d0fb63137932e1d3b1a880e5640c3
Dario Carlino <baires82@gmail.com>
2020-09-21 17:02:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,15 +13,6 @@ class Student
     send("blog=", student_hash[:blog])
     send("bio=", student_hash[:bio])
     send("profile_quote=", student_hash[:profile_quote])
-    # @name = student_hash[:name]
-    # @location = student_hash[:location]
-    # @twitter = student_hash[:twitter]
-    # @linkedin = student_hash[:linkedin]
-    # @github = student_hash[:github]
-    # @blog = student_hash[:blog]
-    # @profile_quote = student_hash[:profile_quote]
-    # @bio = student_hash[:bio]
-    # @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
