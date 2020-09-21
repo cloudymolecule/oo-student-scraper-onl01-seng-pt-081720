627efb9da3966061a07ecee59a9a641aed07b538
Dario Carlino <baires82@gmail.com>
2020-09-21 14:27:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    students = doc.css(".student-card")
+    students = doc.css(".student-card a")
     binding.pry
   end
 
